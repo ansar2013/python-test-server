b69f713e0768ada4826a10591fcf508489d1d800
ansar2013 <s_kundiz@mail.ru>
2025-02-17 01:21:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Сервер жұмыс істеп тұр!"
+import os
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))  # Render автоматты түрде порт береді
+    port = int(os.environ.get("PORT", 5000))  # Render қажетті порты алады
     app.run(host="0.0.0.0", port=port)
